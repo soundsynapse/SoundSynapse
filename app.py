a47527edd88777e6412be662b30a50711517218a
Ryuto Yoda <r.libra6788@gmail.com>
2024-01-25 21:09:42 +0900

    変更2

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+import os
+import re
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
