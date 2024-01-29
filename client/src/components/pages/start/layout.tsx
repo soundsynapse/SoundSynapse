38f03fc0cefc74f7ea97c2ca0b593b652a1139b1
minezaki-mutsuki <mutupg141210@gmail.com>
2024-01-29 17:28:18 +0900

    イラスト追加

--- client/src/components/pages/start/layout.tsx
+++ client/src/components/pages/start/layout.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Button } from "../../parts/button";
+import humanIcon from "../../../../public/humanIcon.png";
 
 type StartLayoutProps = {
   onClickStartButton: () => void;
@@ -8,9 +9,21 @@ type StartLayoutProps = {
 export const StartLayout = ({ onClickStartButton }: StartLayoutProps) => {
   return (
     <Wrapper>
+      <Icon src={humanIcon} alt="イヤホンをした人のイラスト" />
       <Button color="blue" text="はじめる" onClick={onClickStartButton} />
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-top: -100px;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: flex-end;
+`;
+const Icon = styled.img`
+  scale: 0.8;
+  margin-left: auto;
+  margin-right: 10%;
+  margin-top: -250px;
+`;
