9cc31bda97750da82fa9038c0f5573ba5b8a0c6d
minezaki-mutsuki <mutupg141210@gmail.com>
2024-01-29 23:38:19 +0900

    searchButton作成

---
+++ client/src/components/parts/searchButton/index.tsx
@@ -0,0 +1,24 @@
+import { IoIosSearch } from "react-icons/io";
+import styled from "styled-components";
+
+type SearchButtonProps = {
+  onClick: () => void;
+};
+
+export const SearchButton = ({ onClick }: SearchButtonProps) => {
+  return (
+    <Wrapper onClick={onClick}>
+      <IoIosSearch color="#ff13d3" size={40} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.button`
+  background-color: white;
+  border: none;
+  border-radius: 50px;
+  width: 50px;
+  height: 50px;
+  padding-top: 5px;
+  cursor: pointer;
+`;
