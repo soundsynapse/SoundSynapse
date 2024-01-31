acc6e310f0fe3a9bcaa17201d1d4014b8e2e9078
minezaki-mutsuki <mutupg141210@gmail.com>
2024-01-31 23:41:38 +0900

    result画面作成

---
+++ client/src/components/pages/result/index.stories.tsx
@@ -0,0 +1,46 @@
+import { ComponentStory } from "@storybook/react";
+
+import { ResultLayout } from "./layout";
+import { Frame } from "../../parts/frame";
+import { useEffect, useState } from "react";
+import { MusicData } from "../../parts/searchResult";
+
+export default {
+  title: "pages/Result",
+  component: ResultLayout,
+};
+
+export const Template = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Frame>
+      <ResultLayout
+        name={"ユーザーA"}
+        likeMusic={[
+          {
+            id: "4LjIQmt1t6NjpM0tpttzjo",
+            name: "勇者",
+          },
+          {
+            id: "1hAloWiinXLPQUJxrJReb1",
+            name: "アイドル",
+          },
+          {
+            id: "1zd35Y44Blc1CwwVbW3Qnk",
+            name: "群青",
+          },
+        ]}
+        isLoading={isLoading}
+      />
+    </Frame>
+  );
+};
---
+++ client/src/components/pages/result/layout.tsx
@@ -0,0 +1,72 @@
+import { MusicData } from "../../parts/searchResult";
+import loadingIcon from "../../../../public/loading.gif";
+import styled from "styled-components";
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import { MusicCard } from "../../parts/musicCard";
+
+type ResultLayoutProps = {
+  name: string;
+  likeMusic: MusicData[];
+  isLoading: boolean;
+};
+
+export const ResultLayout = ({
+  name,
+  likeMusic,
+  isLoading,
+}: ResultLayoutProps) => {
+  return (
+    <>
+      {isLoading ? (
+        <ImageWrapper>
+          <StyledImage src={loadingIcon} alt="ローディングアイコン" />
+        </ImageWrapper>
+      ) : (
+        <>
+          <ImageWrapper>
+            <HiOutlineUserCircle color="white" size={"250px"} />
+          </ImageWrapper>
+          <Text>
+            {name}さんと
+            <br />
+            マッチングしました♪
+          </Text>
+          <MusicText className="music-text">{name}さんの好きな曲</MusicText>
+          <MusicWrapper>
+            {likeMusic.map((item, index) => (
+              <MusicCard id={item.id} key={index} />
+            ))}
+          </MusicWrapper>
+        </>
+      )}
+    </>
+  );
+};
+
+const StyledImage = styled.img`
+  scale: calc(0.5);
+`;
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: -100px;
+`;
+const Text = styled.h2`
+  margin: 0;
+  font-size: 36px;
+  font-weight: 700;
+  color: white;
+  text-align: center;
+`;
+const MusicText = styled.p`
+  margin: 30px 0;
+  font-size: 24px;
+  color: white;
+  font-weight: 700;
+  text-align: center;
+`;
+const MusicWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+`;
