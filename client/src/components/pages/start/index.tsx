9be24d42f47cda0da0b97ce9a9c793383fe18ab9
minezaki-mutsuki <mutupg141210@gmail.com>
2024-02-03 00:31:33 +0900

    スタート画面ルーティング

---
+++ client/src/components/pages/start/index.tsx
@@ -0,0 +1,8 @@
+import { StartLayout } from "./layout";
+import { useNavigate } from "react-router-dom";
+
+export const Start = () => {
+  const navigate = useNavigate();
+
+  return <StartLayout onClickStartButton={() => navigate("/register")} />;
+};
