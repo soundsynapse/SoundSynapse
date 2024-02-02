3e66feca56ce612ae447c019c3d9ffa3c4330124
minezaki-mutsuki <mutupg141210@gmail.com>
2024-02-02 21:37:22 +0900

    ページネーション作成

---
+++ client/src/components/parts/pagination/index.tsx
@@ -0,0 +1,44 @@
+import { Pagination as PaginationMUI } from "@mui/material";
+import styled from "styled-components";
+
+type PaginationProps = {
+  onClick: (page: number) => void;
+};
+
+export const Pagination = ({ onClick }: PaginationProps) => {
+  return (
+    <StyledPagination
+      style={{ fontSize: "24px", color: "white" }}
+      count={10}
+      onChange={(e, page) => onClick(page)}
+    />
+  );
+};
+
+const StyledPagination = styled(PaginationMUI)({
+  "& .MuiPaginationItem-page, & .MuiPaginationItem-ellipsis": {
+    color: "white", // 数字と「...」の色を白にする
+    fontSize: "24px", // フォントサイズを24pxにする
+  },
+  "& .MuiSvgIcon-root": {
+    fontSize: "24px", // アイコンのサイズを24pxにする
+    color: "white",
+  },
+  "& .MuiPaginationItem-page.Mui-selected": {
+    backgroundColor: "white", // 選択されているページの背景色を青にする
+    color: "#8002db", // 選択されているページの文字色を白にする
+    fontSize: "24px",
+  },
+  "& .MuiPaginationItem-page:hover": {
+    backgroundColor: "rgba(255,255,255,0.4)", // ホバー時の背景色を赤にする
+  },
+  "& .MuiPaginationItem-root:hover": {
+    backgroundColor: "rgba(255,255,255,0.4)",
+  },
+  "& .MuiPaginationItem-ellipsis:hover": {
+    backgroundColor: "transparent",
+  },
+  "& .MuiPaginationItem-page.Mui-selected:hover": {
+    backgroundColor: "white",
+  },
+});
