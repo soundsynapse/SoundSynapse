8b1655acff8a28f56f7dddb2ae1feb01af9239f4
minezaki-mutsuki <mutupg141210@gmail.com>
2024-02-03 14:37:27 +0900

    検索つなぎこみ

--- client/src/components/pages/register/index.tsx
+++ client/src/components/pages/register/index.tsx
@@ -7,6 +7,30 @@ export const Register = () => {
   const [searchResult, setSearchResult] = useState<MusicData[]>([]);
   const navigate = useNavigate();
 
+  const getMusic = async (searchWord: string) => {
+    // try {
+    const res = await fetch(
+      `https://geek-m74g.onrender.com/artist/${searchWord}`,
+      {
+        method: "GET",
+        mode: "cors",
+      }
+    );
+    const result = await res.json();
+    const resultItems: any = result;
+    console.log(result);
+    return resultItems;
+    //} catch (error) {
+    //  alert(error);
+    //}
+  };
+
+  const onSearch = async (searchWord: string) => {
+    const searchResult = await getMusic(searchWord);
+    if (!searchResult) return;
+    setSearchResult(searchResult);
+  };
+
   return (
     <RegisterLayout
       onClickNextButton={(userName, selectedMusic) => {
@@ -15,91 +39,7 @@ export const Register = () => {
         );
         navigate("/result");
       }}
-      onClickSearchButton={(value) => {
-        alert(`検索ワード「${value}」で検索する`);
-        setSearchResult([
-          {
-            id: "4LjIQmt1t6NjpM0tpttzjo",
-            name: "勇者",
-          },
-          {
-            id: "1hAloWiinXLPQUJxrJReb1",
-            name: "アイドル",
-          },
-          {
-            id: "1zd35Y44Blc1CwwVbW3Qnk",
-            name: "群青",
-          },
-          {
-            id: "6MCjmGYlw6mQVWRFVgBRvB",
-            name: "夜に駆ける",
-          },
-          {
-            id: "1VoTe7qVyqyMNLgZpQeugO",
-            name: "祝福",
-          },
-          {
-            id: "4LjIQmt1t6NjpM0tpttzjo",
-            name: "勇者2",
-          },
-          {
-            id: "1hAloWiinXLPQUJxrJReb1",
-            name: "アイドル2",
-          },
-          {
-            id: "1zd35Y44Blc1CwwVbW3Qnk",
-            name: "群青2",
-          },
-          {
-            id: "6MCjmGYlw6mQVWRFVgBRvB",
-            name: "夜に駆ける2",
-          },
-          {
-            id: "1VoTe7qVyqyMNLgZpQeugO",
-            name: "祝福2",
-          },
-          {
-            id: "4LjIQmt1t6NjpM0tpttzjo",
-            name: "勇者3",
-          },
-          {
-            id: "1hAloWiinXLPQUJxrJReb1",
-            name: "アイドル3",
-          },
-          {
-            id: "1zd35Y44Blc1CwwVbW3Qnk",
-            name: "群青3",
-          },
-          {
-            id: "6MCjmGYlw6mQVWRFVgBRvB",
-            name: "夜に駆ける3",
-          },
-          {
-            id: "1VoTe7qVyqyMNLgZpQeugO",
-            name: "祝福3",
-          },
-          {
-            id: "4LjIQmt1t6NjpM0tpttzjo",
-            name: "勇者4",
-          },
-          {
-            id: "1hAloWiinXLPQUJxrJReb1",
-            name: "アイドル4",
-          },
-          {
-            id: "1zd35Y44Blc1CwwVbW3Qnk",
-            name: "群青4",
-          },
-          {
-            id: "6MCjmGYlw6mQVWRFVgBRvB",
-            name: "夜に駆ける4",
-          },
-          {
-            id: "1VoTe7qVyqyMNLgZpQeugO",
-            name: "祝福4",
-          },
-        ]);
-      }}
+      onClickSearchButton={async (value) => await onSearch(value)}
       searchResult={searchResult}
     />
   );
