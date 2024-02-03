35c7750b7bedf585d3da103bcaae4089eecf98b0
nematatu <nematatu1003@gmail.com>
2024-02-03 10:40:30 +0900

    fix: requirement.txt内のopenaiの値、routeの一時退避

--- server/app.py
+++ server/app.py
@@ -3,11 +3,17 @@ import json
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from openai import OpenAI
-
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 app = Flask(__name__)
 
 api_key = "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"
 client = OpenAI(api_key=api_key)
+
+client_id = 'a6bd17dc6fb74c3fba04930d83b6bc94' 
+client_secret = 'f5f882869d3547f3a52216216981bed7' 
+client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 def get_embedding(text, model="text-embedding-ada-002"):
     response = client.embeddings.create(input=[text], model=model)
@@ -33,9 +39,25 @@ def load_and_update_data():
     with open('updated_data.json', 'w', encoding='utf-8') as file:
         json.dump({"data": updated_data}, file, ensure_ascii=False)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+#render_templateが定義されていいなくてエラー出るので一旦退避
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/aritcle/<string:artist>')
+def return_artist(artist):
+    art_name=artist
+    art_search = sp.search(art_name,type='artist')
+    art_id = art_search['artists']['items'][0]['id'] 
+    art_alb = sp.artist_albums(art_id, limit=50,album_type='album') 
+    alb_ids = [alb['id'] for alb in art_alb['items']] 
+    tra_id_name = []
+    for alb_id in alb_ids:
+        tracks = sp.album_tracks(alb_id, limit=50)["items"]
+        for track_num,track in enumerate(tracks):
+            dict = {'name':track["name"],'id':track["id"]}
+            tra_id_name.append(dict)
+    return tra_id_name
 
 @app.route('/recommendations', methods=['GET'])
 def get_recommendations():
