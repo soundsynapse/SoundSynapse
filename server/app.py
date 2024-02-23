a45cddd31801f80a048f43b9de4878b7e0b0eba9
nematatu <nematatu1003@gmail.com>
2024-02-23 18:49:01 +0900

    db.pyの追加

--- server/app.py
+++ server/app.py
@@ -1,14 +1,33 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask
 from flask_cors import CORS
+import os
 
-app = Flask(__name__)
-CORS(app)
 
-@app.route('/')
-def index():
-     return render_template('index.html')
 
-app.json.ensure_ascii = False        
+def create_app(test_config=None):
+    app=Flask(__name__,instance_relative_config=True)
+    CORS(app)
+    app.json.ensure_ascii=False
+    
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path,"flaskr.sqlite")
+    )
+    if test_config is None:
+        app.config.from_pyfile("config.py",silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        os.mkdir(app.instance_path)
+    except OSError:
+        pass
+    
+    from . import db
+    db.init_app(app)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    
+    return app
