77c938d293b85fb6b8b4897d434e925d91d32440
nematatu <nematatu1003@gmail.com>
2024-02-25 06:29:44 +0900

    setup

--- server/schema.sql
+++ server/schema.sql
@@ -20,5 +20,5 @@ CREATE TABLE post(
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES "user" (id)
+    FOREIGN KEY (author_id) REFERENCES username (id)
 );
