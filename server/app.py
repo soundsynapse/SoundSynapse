660c7ee4e8e8b6594dbd7ca12f45ea361c446dbf
nematatu <nematatu1003@gmail.com>
2024-02-25 05:35:51 +0900

    success

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-from flask import Flask,g,current_app
+from flask import Flask
 from flask_cors import CORS
 import os
-import psycopg2
 
 app = Flask(__name__, instance_relative_config=True)
 CORS(app)
@@ -9,53 +8,21 @@ app.json.ensure_ascii = False
 
 app.config.from_mapping(
     SECRET_KEY="dev",
-    DB=os.environ["DATABASE_URL"],
+    DATABASE=os.environ["DATABASE_URL"],
 )
-
 
 # from feature import db
 # db.init_db()
 # from feature import auth
 # app.register_blueprint(auth.bp)
 
-from feature import test
 
-
-# def get_db():
-#     if not hasattr(g,"pg_conn"):
-#         g.pg_conn = psycopg2.connect(
-#             database_url
-#         )
-#     return g.pg_conn
-
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello World!'
-# @app.route('/')
-# def hello_world():
-#     conn=db.get_db()
-#     cursor=conn.cursor()
+    return "init-db()"
 
-#     cursor.execute('CREATE TABLE IF NOT EXISTS visits (visited_on TIMESTAMP)')
-#     conn.commit()
 
-#     return 'CREATE TABLE!'
-
-# @app.route('/visit')
-# def visit():
-#     conn=db.get_db()
-#     cursor=conn.cursor()
-
-#     cursor.execute('INSERT INTO visits VALUES (NOW())')
-#     conn.commit()
-
-#     cursor.execute('SELECT * FROM visits')
-
-#     row=cursor.fetchall()
-#     result=','.join([str(r) for r in row])
-    
-#     return 'DataBase content:'+result
-
+from feature import test
 
 app.register_blueprint(test.app)
 
