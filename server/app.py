edfff0d581ab7fa5532ee9a4d40a823c522e4ebf
nematatu <nematatu1003@gmail.com>
2024-02-25 05:00:35 +0900

    config

--- server/app.py
+++ server/app.py
@@ -2,27 +2,30 @@ from flask import Flask
 from flask_cors import CORS
 import os
 
-app=Flask(__name__,instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 CORS(app)
-app.json.ensure_ascii=False
+app.json.ensure_ascii = False
 
 app.config.from_mapping(
     SECRET_KEY="dev",
-    DATABASE=os.environ['DATABASE_URL'],
+    DATABASE=os.environ["DATABASE_URL"],
 )
 
-from feature import db
-db.init_db()
+# from feature import db
+# db.init_db()
 # from feature import auth
 # app.register_blueprint(auth.bp)
 
 
 @app.route("/")
 def hello():
-     return "init-db()"
+    return "init-db()"
+
 
 from feature import test
-app.register_blueprint(test.app)
 
-if __name__ == '__main__':
+app.register_blueprint(test.app)
+test.init_test()
+
+if __name__ == "__main__":
     app.run()
