c7b4e4d615bb94cda534bce5ba5a9c3ecc679bf9
nematatu <nematatu1003@gmail.com>
2024-02-25 07:30:13 +0900

    %s

--- server/feature/auth.py
+++ server/feature/auth.py
@@ -106,7 +106,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = (
-            get_db().execute("SELECT * FROM user WHERE id=?", (user_id,)).fetchone()
+            get_db().execute("SELECT * FROM user WHERE id=%s", (user_id,)).fetchone()
         )
 
 @bp.route("/logout")
