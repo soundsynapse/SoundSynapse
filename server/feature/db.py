6ef9272336c20f88fe582d4ebc081b95d351503d
nematatu <nematatu1003@gmail.com>
2024-02-25 04:55:28 +0900

    config

--- server/feature/db.py
+++ server/feature/db.py
@@ -43,14 +43,14 @@ def init_db():
                 cursor.execute(statement)
     db.commit()
 
-@click.command("init-db")
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    close_db()
-    init_db()
-    click.echo("Initialized the database.")
+# @click.command("init-db")
+# def init_db_command():
+#     """Clear the existing data and create new tables."""
+#     close_db()
+#     init_db()
+#     click.echo("Initialized the database.")
 
 
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+# def init_app(app):
+#     app.teardown_appcontext(close_db)
+#     app.cli.add_command(init_db_command)
