9353a06526207f07c35fad312ed70f5dfc8a009c
nematatu <nematatu1003@gmail.com>
2024-02-25 04:06:57 +0900

    schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -1,27 +1,28 @@
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS "user" CASCADE;
+DROP TABLE IF EXISTS oauth CASCADE;
+DROP TABLE IF EXISTS post CASCADE;
 
-CREATE TABLE user(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE username(
+    id SERIAL PRIMARY KEY,
     userid TEXT UNIQUE NOT NULL,
     icon_url TEXT DEFAULT 'https://soco-st.com/wp-content/themes/socost/upload/18225_color.svg',
     name TEXT NOT NULL
 );
 
 CREATE TABLE oauth(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTERGER,
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
     identify_type TEXT NOT NULL,
     identifier TEXT NOT NULL,
     credential TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id)
-    );
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
+);
 
 CREATE TABLE post(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     author_id INTEGER NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES user (id)
-)
+    FOREIGN KEY (author_id) REFERENCES "user" (id)
+);
